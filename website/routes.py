ac9f1ab97273fc01aef475063246a09d331575fc
Valentin Urena Baltazar <valentin.urena@gmail.com>
2026-01-11 14:55:31 -0800

    updates, refactring and cleanig up README

--- website/routes.py
+++ website/routes.py
@@ -8,6 +8,7 @@ from sklearn.metrics.pairwise import cosine_similarity, euclidean_distances
 from sentence_transformers import SentenceTransformer
 import lyricsgenius as genius
 
+DEVICE = "cpu"
 
 routes = Blueprint('routes', __name__)
 
@@ -141,9 +142,9 @@ def make_playlist():
     user_input = request.form['user_input']
 
     df = pd.read_csv('./website/static/data/rec_data.csv')
-    model = SentenceTransformer('bert-base-nli-mean-tokens',device='cuda')
+    model = SentenceTransformer('bert-base-nli-mean-tokens',device=DEVICE)
     corpus_embeddings = np.load('./website/static/data/corpus_embeddings.npy')
-    
+
     if user_input != "None":
 
         print(user_input)
@@ -175,25 +176,25 @@ def play_song(track_id):
 
 @routes.route('/vibe_demo', methods=['POST'])
 def random_song_demo():
+
     file_name = request.form['file_name']
     print('file name is:', file_name)
 
     cluster_number = int(file_name.split('.')[0].split('_')[-1])
     print("cluster is number:",cluster_number)
     
-
     df = pd.read_csv('./website/static/data/demo.csv')
-    print(df.head())
+    # print(df.head())
 
     df_cluster = df[df['cluster']==cluster_number]
     
-    random_row = df_cluster.sample(n=1)
+    random_row = df_cluster.sample(n=3)
     
-    song_name = random_row['song_title'].values[0]
-    artist_name = random_row['artist_name'].values[0]
+    # song_name = random_row['song_title'].values[0]
+    # artist_name = random_row['artist_name'].values[0]
 
-    print(str(song_name))
-    print(str(artist_name))
+    # print(str(song_name))
+    # print(str(artist_name))
     
     CLIENT_ID = '9e037a45a23c4f22a1df30d1820bae65'
     CLIENT_SECRET = '88412132bc70474e8ade57eb6c080039'
@@ -206,15 +207,24 @@ def random_song_demo():
     current_directory = os.getcwd()
     print("Current directory:", current_directory)
 
+    for i in range(len(random_row)):
+        song_name = random_row['song_title'].values[i]
+        artist_name = random_row['artist_name'].values[i]
+        try:
+            # Search for the song
+            res = sp.search(q=f"track:{song_name} artist:{artist_name}", type='track', limit=1)
+            # print(res)
+            if res['tracks']['items']:
+                # Get the first track found
+                track_id = res['tracks']['items'][0]['id']
+                print(track_id)
+                # return redirect(url_for('play', track_id=track_id))
+                return redirect(url_for('routes.play_song', track_id=track_id))
+            # else:
+                # return "Song not found."
+                # ret random_song_demo()
+                # return redirect(url_for('routes.random_song_demo', file_name_try=file_name))
+        except Exception as e:
+            print("An error occurred:", e)
+            continue
 
-    # Search for the song
-    res = sp.search(q=f"track:{song_name} artist:{artist_name}", type='track', limit=1)
-    # print(res)
-    if res['tracks']['items']:
-        # Get the first track found
-        track_id = res['tracks']['items'][0]['id']
-        print(track_id)
-        # return redirect(url_for('play', track_id=track_id))
-        return redirect(url_for('routes.play_song', track_id=track_id))
-    else:
-        return "Song not found."
